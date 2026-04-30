4cf98e7f218678b0ed8b8be438661129ebbcf390
TufanRai <thulungtufan16@gmail.com>
2026-04-30 15:18:53 +0545

    Learned to take input from the users

--- Main.java
+++ Main.java
@@ -1,13 +1,57 @@
+
+import java.util.Scanner;
+
 // public accessibility of the class
 // class
 // Main (name of class)
-
 public class Main {
 
     public static void main(String[] arg) {
-        System.out.println("Hello, world!");
+        Scanner scanner = new Scanner(System.in);
+        /*
+            Primitive Datatypes 
+            -------------------
+            -------------------
+            1. Boolean (default value: false)
+            2. Character (default value: /u000000)
+            3. byte (default value: 0) Small integer can hold up to 1byte
+            4. short (default value: 0) Little greater integer can hold up to 2byte
+            5. int (default value: 0) Larger than short, can hold up to 4byte
+            6. long (default value: 0) Larger than int, can hold up to 8byte
+            7. float (deafult value: 0f) Floating point number, can hold up to 4byte
+            8. double (default value: 0d) Larger than floating point number, can hold up to 8byte
 
-        int age = 10;
-        System.out.println(age);
+            ------------------
+            ------------------
+            Selecting the perfect datatype is the best way to save your memory and make your code faster.
+            ------------------
+            ------------------
+            NOTE:-
+            Whenever you declare a variable with float datatype, by default it assums itself as a double. 
+            Thus, we do have to mention f at the end to identify as a double.
+         */
+
+        // WAP to find the greatest number between three numbers.
+        byte num1 = 12;
+        byte num2 = 24;
+        byte num3 = 23;
+
+        if (num1 > num2 && num1 > num3) {
+            System.out.println("The greates number is " + num1);
+        } else if (num2 > num1 && num2 > num3) {
+            System.out.println("The greates number is " + num2);
+        } else {
+            System.out.println("The greates number is " + num3);
+        }
+
+        // WAP to take user's name and age and display them as an output. 
+        System.out.println("Enter your name");
+        String name = scanner.nextLine();
+        System.out.println("Enter your age");
+        byte age = scanner.nextByte();
+
+        scanner.close();
+
+        System.out.println("Your name is " + name + " and you are " + age + " years old.");
     }
 }
