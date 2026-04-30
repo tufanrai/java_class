d65be63b3c0df13fcba8c0f585d243b0b1e4fb71
TufanRai <thulungtufan16@gmail.com>
2026-04-30 14:31:48 +0545

    created a java main code file

---
+++ Main.java
@@ -0,0 +1,13 @@
+// public accessibility of the class
+// class
+// Main (name of class)
+
+public class Main {
+
+    public static void main(String[] arg) {
+        System.out.println("Hello, world!");
+
+        int age = 10;
+        System.out.println(age);
+    }
+}
