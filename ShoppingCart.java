5f82bda279c61f16387bbbfa462cd07b249eea21
TufanRai <thulungtufan16@gmail.com>
2026-04-30 16:25:25 +0545

    created a simple billing system using switch case

---
+++ ShoppingCart.java
@@ -0,0 +1,42 @@
+
+import java.util.Scanner;
+
+public class ShoppingCart {
+
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            String itemName;
+            byte price;
+            byte quantity;
+            short total;
+
+            System.out.print("Enter the name of the food Item: ");
+            itemName = scanner.next();
+
+            price = switch (itemName) {
+                case "pizza" ->
+                    120;
+
+                case "burger" ->
+                    100;
+
+                case "momo" ->
+                    120;
+
+                default ->
+                    0;
+            };
+
+            System.out.print("Enter the quantity of the food item: ");
+            quantity = scanner.nextByte();
+
+            total = (short) (price * quantity);
+
+            System.out.println("\nitem: " + itemName + "\nprice: " + price + "\nquantity " + quantity + "\ntotal: " + total);
+
+            scanner.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
